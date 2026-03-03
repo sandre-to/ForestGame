cdf8a93f4f4743d05f03004fad7cc9a3ca11874c
sandre-to <sandre.mnto@gmail.com>
2026-03-03 21:36:53 +0100

    Add health bar

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -1,9 +1,15 @@
 using Godot;
-using System;
 
 namespace Scenes;
 [GlobalClass]
 public partial class Main : Node3D
 {
+    private Node3D _trees;
+    private CsgBox3D _ground;
 
+    public override void _Ready()
+    {
+        _trees = GetNode<Node3D>("Trees");
+        _ground = GetNode<CsgBox3D>("%Floor");
+    }
 }
